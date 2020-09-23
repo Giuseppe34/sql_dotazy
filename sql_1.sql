8d13c72b4cfb2e6b6ddfe54734d6b760d4bedc2e
Giuseppe34 <32055516+Giuseppe34@users.noreply.github.com>
2020-09-23 09:53:22 +0200

    1 část hotovo

--- sql_1.sql
+++ sql_1.sql
@@ -54,22 +54,63 @@ select distinct customer_id
 from rental 
 where return_date is null
 
-/* 17 */
+/*14 - rozdělení data na den, mesic, rok*/
+select year(rental_date) as rok, month(rental_date) as mesic, day(rental_date) as den
+from rental
+
+/*15 - delka řetězce*/
+select title
+from film
+where len(title) < 20 
+
+
+/*16 - výběr minut z datumu (1440 minut v jednom dni)*/
+select (rental_duration)*1440 as doba_vypujcky_v_min
+from film
+
+
+/* 17 spojeni dvou sloupcu do jednoho, jde to i s CONCAT*/
 select first_name + ' ' + last_name as fullname
 from customer
 where active = 1
+
+/*18 když chci doplnit misto null nějakou hodnotu*/
+select ISNULL(postal_code, 'psc')
+from address
+
+/*19 spojení s CONCAT*/
+select CONCAT(rental_date, + ' - ' ,return_date) as doba
+from rental  
+where return_date is not null
+
+/*20 ?*/
+select CONCAT(ISNULL(return_date, rental_date), + rental_date + '-', return_date) as xd
+from rental  
 
 /* 21 */
 select count(*) as pocet_filmu
 from film
 
-/* 22 */
+/* 22 distinct = jen jedno z každého (když se něco opakuje, tak to beru jen jendou) */
 select COUNT (distinct rating) as pocet_klas
 from film
 
 /* 23 */
 select 
 	count(*) as post_code,
-	count(distinct postal_code) as pocet_ruznych_psc,
-	count(postal_code) as pocet_adres_s_psc
+	count(distinct postal_code) as pocet_vyplnenych_psc
+	count(postal_code) as pocet_ruznych_psc
 from address
+
+/*24 min, max, avg*/
+select AVG(length) as prumer, min(length) as min, max(length) as max, sum(length)
+from film
+
+/*25*/
+select count(payment_id) as pocet, sum(amount) as soucet
+from payment
+where year(payment_date) = 2005
+
+/*26 pocet písmen v názvech*/
+select SUM(LEN(title)) as pocet_pismen_v_nazvech
+from film
